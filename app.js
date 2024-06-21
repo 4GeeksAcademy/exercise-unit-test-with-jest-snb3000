d0800170ff50d51fe017aca594f29508f1294343
Shafik Bahou <snb3000@gmail.com>
2024-06-21 23:42:25 +0000

    add additional conversions and made sure the tests passed

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-// This is my function that sums two numbers
-const sum = (a,b) => {
-    return a + b
+// Function to sum two numbers
+const sum = (a, b) => {
+    return a + b;
 }
 
-// Just a console log for ourselves
-console.log(sum(7,3))
+// Conversion rates
+const conversionRates = {
+    "JPY": 156.5, // Japanese Yen
+    "USD": 1.07, // US Dollar
+    "GBP": 0.87 // British Pound
+};
 
-// Export the function to be used on other files 
-// (similar to the keyword "export" when using webpack)
-module.exports = { sum };
+// Function to convert from Euro to Dollar
+const fromEuroToDollar = function(valueInEuro) {
+    const valueInDollar = valueInEuro * 1.07;
+    return valueInDollar;
+}
+
+// Function to convert from Dollar to Yen
+const fromDollartoYen = (amountInDollars) => {
+    const amountInEuros = amountInDollars / conversionRates.USD;
+    return amountInEuros * conversionRates.JPY;
+}
+
+// Function to convert from Yen to Pound
+const fromYentoPound = (amountInYen) => {
+    const amountInEuros = amountInYen / conversionRates.JPY;
+    return amountInEuros * conversionRates.GBP;
+}
+
+module.exports = { sum, fromEuroToDollar, fromDollartoYen, fromYentoPound };
+
--- test.js
+++ test.js
@@ -1,11 +1,27 @@
-// Import the function sum from the app.js file
-const { sum } = require('./app.js');
+const { sum, fromEuroToDollar, fromDollartoYen, fromYentoPound } = require('./app.js');
 
-// Start your first test
 test('adds 14 + 9 to equal 23', () => {
-    // Inside the test we call our sum function with 2 numbers
-    let total = sum(14, 9);
-
-    // We expect the sum of those 2 numbers to be 23
+    const total = sum(14, 9);
     expect(total).toBe(23);
+});
+
+test("One euro should be 1.07 dollars", () => {
+    const euros = 3.5;
+    const dollars = fromEuroToDollar(euros);
+    const expected = euros * 1.07;
+    expect(dollars).toBe(expected);
+});
+
+test("converts dollars to yen correctly", () => {
+    const dollars = 1;
+    const yen = fromDollartoYen(dollars);
+    const expected = dollars / 1.07 * 156.5;
+    expect(yen).toBe(expected);
+});
+
+test("converts yen to pounds correctly", () => {
+    const yen = 156.5;
+    const pounds = fromYentoPound(yen);
+    const expected = yen / 156.5 * 0.87;
+    expect(pounds).toBe(expected);
 });
